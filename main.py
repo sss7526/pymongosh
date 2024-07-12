55e508ad408457e6d29029c69062b3cc2c4ec84c
SSS <142176901+sss7526@users.noreply.github.com>
2024-07-11 22:53:47 -0400

    Create main.py
    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+from mongo_shell import MongoShell
+from shell import InteractiveShell
+
+def main():
+    uri = "mongodb://localhost:27017"
+    mongo_shell = MongoShell(uri)
+    shell = InteractiveShell(mongo_shell)
+    shell.start()
+
+if __name__ == '__main__':
+    main()
